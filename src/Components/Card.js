cbf117a521bc6d8a788d23b1d281ca805001fce7
enhao25 <larloller@gmail.com>
2020-03-07 09:39:16 +0800

    Update to data

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, weather, id }) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            {weather < 27 
+            {weather < 28 
                 ? <img alt="robot_photo" src={require('./cloud.png')} height="200" width="200" />
                 : <img alt="robot_photo" src={require('./sunny.png')} height="200" width="200" />}
             
